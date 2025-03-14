4ed60acd6635b5f4340cc59c800a7fee52e62034
Elida3333 <elida.ekelund@elev.ga.lbs.se>
2025-03-14 14:44:06 +0100

    shoot

--- SlutProject/Assets/Elida/Scripts/Bullet.cs
+++ SlutProject/Assets/Elida/Scripts/Bullet.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   
+
+  
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnBecameInvisible()
     {
-        
+        Destroy(gameObject);
     }
+
+
+  
 }
--- SlutProject/Assets/Elida/Scripts/PlayerShotting.cs
+++ SlutProject/Assets/Elida/Scripts/PlayerShotting.cs
@@ -10,12 +10,13 @@ public class PlayerShotting : MonoBehaviour
     [SerializeField] GameObject Bullet;
     void Start()
     {
-        
+       
     }
 
     void OnShoot()
     {
         GameObject bullet = Instantiate(Bullet, transform.position, transform.rotation);
+        Debug.Log("Shoot");
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(transform.up * Bulletspeed, ForceMode2D.Impulse);
     }
