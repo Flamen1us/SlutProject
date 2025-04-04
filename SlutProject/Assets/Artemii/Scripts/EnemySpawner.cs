bac3197ab3abfb93a4a043e7f85c9b4e4d69dd93
Artemii_Polishchuk <ilovecrowandsendy@gmail.com>
2025-04-04 14:02:08 +0200

    MainMeny/Hud och Test

--- SlutProject/Assets/Artemii/Scripts/EnemySpawner.cs
+++ SlutProject/Assets/Artemii/Scripts/EnemySpawner.cs
@@ -8,6 +8,10 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] float minSpawnTime = 1f;
     [SerializeField] float maxSpawnTime = 3f;
     [SerializeField] float spawnDistance = 10f;
+    [SerializeField] float spawnLeftBorderX = 0;
+    [SerializeField] float spawnRightBorderX = 0;
+    [SerializeField] float spawnUpBorder = 0;
+    [SerializeField] float spawnDownBorder = 0;
     Vector2 spawnPos;
     // Start is called before the first frame update
     void Start()
@@ -24,20 +28,16 @@ public class EnemySpawner : MonoBehaviour
         switch (side)
         {
             case 0:
-                spawnPos = new Vector2(Random.Range(-21.6f, 42), 8f + spawnDistance);
-                Debug.Log(side);
+                spawnPos = new Vector2(Random.Range(spawnLeftBorderX, spawnRightBorderX), spawnUpBorder + spawnDistance);
                 break;
             case 1:
-                spawnPos = new Vector2(Random.Range(-21.6f, 42), -7f - spawnDistance);
-                Debug.Log(side);
+                spawnPos = new Vector2(Random.Range(spawnLeftBorderX, spawnRightBorderX), spawnDownBorder - spawnDistance);
                 break;
             case 2:
-                spawnPos = new Vector2(42 - spawnDistance, Random.Range(-7f, 8f));
-                Debug.Log(side);
+                spawnPos = new Vector2(spawnLeftBorderX - spawnDistance, Random.Range(spawnDownBorder, spawnUpBorder));
                 break;
             case 3:
-                spawnPos = new Vector2(-21.6f + spawnDistance, Random.Range(-7f, 8f));
-                Debug.Log(side);
+                spawnPos = new Vector2(spawnRightBorderX + spawnDistance, Random.Range(spawnDownBorder, spawnUpBorder));
                 break;
         }
         Instantiate(enemyprefab, spawnPos, transform.rotation);
--- SlutProject/Assets/Elida/Scripts/Bullet.cs
+++ SlutProject/Assets/Elida/Scripts/Bullet.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-   
 
+    [SerializeField] GameObject coin;
   
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(collision.gameObject);
+            Instantiate(coin, collision.transform.position, collision.transform.rotation);
         }
         Destroy(gameObject);
     }
