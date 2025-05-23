1fa8023c24d080f7c83f0006115826b913860ef9
Elida3333 <elida.ekelund@elev.ga.lbs.se>
2025-05-23 19:34:39 +0200

    upgrades fixt

--- SlutProject/Assets/Elida/Scenes/Store/Upgardes.cs
+++ SlutProject/Assets/Elida/Scenes/Store/Upgardes.cs
@@ -10,32 +10,74 @@ public class Upgardes : MonoBehaviour
     public int Healthup = 5;
     public int Damageup = 2;
 
+
+    public int costSpeed = 10;
+    public int costHealth = 20;
+    public int costDamage = 30;
+
+    private Stats stats;
+
     void Start()
     {
-        
+        stats = FindObjectOfType<Stats>();
     }
 
    
     void Update()
     {
-        
+        if(stats != null)
+        {
+            upgrade1.interactable = stats.coins >= costSpeed;
+            upgrade2.interactable = stats.coins >= costHealth;
+            upgrade3.interactable = stats.coins >= costDamage;
+        }
     }
 
     public void BuyBulletspeed()
     {
-        Debug.Log("Bought");
-        FindObjectOfType<PlayerShotting>().Bulletspeed += speedtoadd;
+        Stats stats = FindObjectOfType<Stats>();
+        if(stats.coins >= costSpeed)
+        {
+            stats.coins -= costSpeed;
+            Debug.Log("Bought");
+            FindObjectOfType<PlayerShotting>().Bulletspeed += speedtoadd;
+        }
+        else
+        {
+            Debug.Log("Not enough coins");
+        }
+       
     }
 
     public void BuyHealth()
     {
-        Debug.Log("Buy");
-        FindObjectOfType<Stats>().playerHealth += Healthup;
+        Stats stats = FindObjectOfType<Stats>();
+        if(stats.coins >= costHealth)
+        {
+            stats.coins -= costHealth;
+            Debug.Log("Bought");
+            FindObjectOfType<Stats>().playerHealth += Healthup;
+        }
+        else
+        {
+            Debug.Log("Not enough coins");
+        }
+       
     }
 
     public void BuyEnemyDamage()
     {
-        Debug.Log("Bought");
-        FindObjectOfType<Stats>().damage += Damageup;
+        Stats stats = FindObjectOfType<Stats>();
+        if(stats.coins >= costDamage)
+        {
+            stats.coins -= costDamage;
+            Debug.Log("Bought");
+            FindObjectOfType<Stats>().damage += Damageup;
+        }
+        else
+        {
+            Debug.Log("Not enough coins");
+        }
+       
     }
 }
